4c5fa616f25dc9bd275e8766e1bf6b093f35ad1d
elatelation <36717206+elatelation@users.noreply.github.com>
2021-03-10 20:02:42 +0000

    be lazy looking up track artist

--- src/library/mp3.rs
+++ src/library/mp3.rs
@@ -13,7 +13,7 @@ fn parse_tags(t: Tag, p: PathBuf) -> Option<Track> {
         title: t.title()?.to_owned(),
         track_no: t.track()?,
         album: t.album()?.to_owned(),
-        album_artist: t.album_artist().or(t.artist())?.to_owned(),
+        album_artist: t.album_artist().or_else(|| t.artist())?.to_owned(),
         path: p,
     })
 }
