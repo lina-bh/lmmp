ce90e450dd7ffa16d3e37015f888459bb0507ac5
elatelation <36717206+elatelation@users.noreply.github.com>
2021-03-14 13:05:15 +0000

    add libavformat based parser
    really slow at doing mp3s and m4as. but we have backups for that

---
+++ src/library/ffmpeg.rs
@@ -0,0 +1,41 @@
+use super::{Parser, Track};
+use anyhow::Result;
+use ffmpeg_next as ffmpeg;
+use std::path::Path;
+
+pub struct AVFParser;
+
+impl Parser for AVFParser {
+    fn parse(path: impl AsRef<Path>) -> Result<Option<Track>> {
+        let avf = match ffmpeg::format::input(&path) {
+            Ok(f) => f,
+            Err(e) => match e {
+                ffmpeg::Error::InvalidData => return Ok(None),
+                u => return Err(u.into()),
+            },
+        };
+        let met = avf.metadata();
+        Ok(parse_tags(&met, path.as_ref()))
+    }
+}
+
+fn parse_tags(m: &ffmpeg::DictionaryRef, p: &Path) -> Option<Track> {
+    let get = |key| m.get(key).map(|val| val.to_owned());
+    let track_total = get("track")?;
+    let track = track_total
+        .split("/")
+        .take(1)
+        .collect::<Vec<&str>>()
+        .get(0)
+        .and_then(|n| n.parse::<u32>().ok())?;
+    let title = get("title")?;
+    let album = get("album")?;
+    let artist = get("album_artist").or_else(|| get("artist"))?;
+    Some(Track {
+        path: p.to_owned(),
+        title,
+        album,
+        album_artist: artist,
+        track_no: track,
+    })
+}
