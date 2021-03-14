37d57025579dbabbe52ae5a4155e112c162de386
elatelation <36717206+elatelation@users.noreply.github.com>
2021-03-14 13:06:59 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 mod gl;
 mod library;
-mod widgets;
+// mod vorbfile;
 mod window;
 
+use ffmpeg_next as ffmpeg;
 use std::env;
 use window::LmmpWindow;
 
+fn set_ffmpeg_loglevel() {
+    use ffmpeg::util::log;
+
+    log::set_level(log::Level::Warning);
+}
+
 fn main() {
+    set_ffmpeg_loglevel();
+
+    // return;
     let library_path = String::from("~/Music");
     let library_path = library_path.replace("~", &env::var("HOME").unwrap());
     let files = library::index(&library_path).unwrap();
+    // for f in files {
+    //     println!("{:?}", f);
+    // }
+    return;
 
-    library::vorb::_test();
+    // library::vorb::_test();
 
     let window = LmmpWindow::new();
     window.run();
