8db2ae75591a652c945b5a91273c13ec89c1b980
elatelation <36717206+elatelation@users.noreply.github.com>
2021-03-20 19:57:25 +0000

    be a bit more hygienic with the gl bindings

--- src/gl.rs
+++ src/gl.rs
@@ -1,5 +1,8 @@
 #![allow(unused_mut)]
 #![allow(bare_trait_objects)]
 #![allow(clippy::all)]
+mod gl_raw {
+    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+}
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub use gl_raw::*;
