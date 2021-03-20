6022368f1f5c54303d64e9ffb2d9a85a72818698
elatelation <36717206+elatelation@users.noreply.github.com>
2021-03-20 21:01:27 +0000

    tidy up buildscript

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-extern crate gl_generator;
-
 use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 use std::env;
 use std::fs::File;
@@ -8,11 +6,16 @@ use std::path::Path;
 fn main() {
     #[cfg(feature = "ui")]
     {
-        let dest = env::var("OUT_DIR").unwrap();
-        let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
-
-        Registry::new(Api::Gl, (3, 0), Profile::Core, Fallbacks::All, [])
-            .write_bindings(GlobalGenerator, &mut file)
-            .unwrap();
+        generate_gl_bindings();
     }
+}
+
+#[allow(dead_code)]
+fn generate_gl_bindings() {
+    let dest = env::var("OUT_DIR").unwrap();
+    let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
+
+    Registry::new(Api::Gl, (3, 0), Profile::Core, Fallbacks::All, [])
+        .write_bindings(GlobalGenerator, &mut file)
+        .unwrap();
 }
